e4a951bc0cecbf203b46bbf0f2ff30a734d0fa91
Vikash Kumar02 <vikash.kumar2@factset.com>
2018-05-03 23:01:29 +0530

    changed static files

--- plyproject/settings.py
+++ plyproject/settings.py
@@ -124,6 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/loginModule/'
